1db69e0e434f7de34f0f6e149d29ea3b908fda0f
ProcessingDeptartment <michaela@abagold.co.za>
2026-08-21 09:35:40 +0200

    Update gallery.js

--- public/gallery.js
+++ public/gallery.js
@@ -83,7 +83,7 @@ function displayGalleryItems() {
   galleryGrid.innerHTML = filteredItems.map(item => `
     <div class="gallery-item" data-gallery-id="${item.id}">
       <div class="gallery-image">
-        <img src="${item.image}" alt="${item.title}" style="width:100%; height:100%; object-fit:cover;">
+        <img src="${item.image || item.imageUrl}" alt="${item.title}" style="width:100%; height:100%; object-fit:cover; display:block;">
       </div>
       <div class="gallery-info">
         <div class="gallery-category">${item.category}</div>
